4099c88cf9b7611950dda69da2f090b6a84dd184
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-10 22:05:00 +0300

    Intialized variables in Javascript

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//Intializing variables
+const APIgender = "https://api.genderize.io?name=";
+const APIage ="https://api.agify.io/?name=";
+const APInationality = "https://api.nationalize.io/?name=";
+const dogImage = "https://dog.ceo/api/breeds/image/random";
+const button = document.getElementById("submit");
